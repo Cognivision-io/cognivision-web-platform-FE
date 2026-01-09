8e9106490c24f0d7027c622ee586065a5c33d10b
TheMODventures <themodventures2023@gmail.com>
2026-01-09 18:53:51 +0500

    Update use-debounce.ts

--- src/hooks/use-debounce.ts
+++ src/hooks/use-debounce.ts
@@ -2,12 +2,10 @@ import { useEffect, useState } from "react";
 
 export function useDebounce<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
-
   useEffect(() => {
     const timer = setTimeout(() => {
       setDebouncedValue(value);
     }, delay);
-
     return () => {
       clearTimeout(timer);
     };
