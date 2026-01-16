0481ad1784f457d4b5a8a6e23002f2c5df609531
TheMODventures <themodventures2023@gmail.com>
2026-01-16 17:21:40 +0500

    Update contact.mutation.ts

--- src/features/contact/mutations/contact.mutation.ts
+++ src/features/contact/mutations/contact.mutation.ts
@@ -8,7 +8,6 @@ import type {
 } from "@/interfaces/contact.interface";
 
 export type ContactError = AxiosError<{ message?: string | string[] }>;
-
 export const CONTACT_US_MUTATION_KEY = ["contact", "contact-us"] as const;
 
 export const useContactUsMutation = (
