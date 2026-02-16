141edf171389cda5a19f47dbbbb525159ef0cceb
Muhammad Usman <64627750+usmanthesuper@users.noreply.github.com>
2026-02-16 22:01:09 +0500

    url fixed

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosHeaders } from "axios";
 
 // Create axios instance with default config
 const api = axios.create({
-  baseURL: "https://modflow-backend-dev.yameenyousuf.com",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
